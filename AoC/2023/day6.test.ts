d26f0da5afd42d06dd5ccec109bd936e51ec146b
jessejp <jessepernila@gmail.com>
2023-12-06 15:04:01 +0200

    AoC 2023 day 6 part 2 done

--- AoC/2023/day6.test.ts
+++ AoC/2023/day6.test.ts
@@ -5,7 +5,6 @@ const exampleFile = Bun.file("./AoC/2023/input/day6_example.txt");
 const inputFile = Bun.file("./AoC/2023/input/day6.txt");
 
 type raceHighscore = {
-  id: number;
   time: number;
   distance: number;
 };
@@ -19,40 +18,33 @@ const getInputList = async (file: BunFile) => {
 
   if (!raceHighscoreColumn.length) return;
 
-  raceHighscoreColumn[0]
-    .slice(1)
-    .forEach((row, index) => {
-      raceHighscoreList.push({
-        id: index,
-        time: Number(row),
-        //offset index by 1 due to slice
-        distance: Number(raceHighscoreColumn[1][index + 1]),
-      });
+  raceHighscoreColumn[0].slice(1).forEach((row, index) => {
+    raceHighscoreList.push({
+      time: Number(row),
+      //offset index by 1 due to slice
+      distance: Number(raceHighscoreColumn[1][index + 1]),
     });
+  });
   return raceHighscoreList;
 };
 
-// const getPart2InputList = async (file: BunFile) => {
-//     const raceHighscoreList: raceHighscore[] = [];
-//     const fileAsText = await file.text();
-//     const raceHighscoreColumn = fileAsText
-//       .split("\n")
-//       .map((row) => row.split(" ").filter((f) => !!f).slice(1));
-  
-//     if (!raceHighscoreColumn.length) return;
-  
-//     raceHighscoreColumn[0]
-//       .slice(1)
-//       .forEach((row, index) => {
-//         raceHighscoreList.push({
-//           id: index,
-//           time: Number(row),
-//           //offset index by 1 due to slice
-//           distance: Number(raceHighscoreColumn[1][index + 1]),
-//         });
-//       });
-//     return raceHighscoreList;
-//   };
+const getPart2InputList = async (file: BunFile): Promise<raceHighscore> => {
+  const fileAsText = await file.text();
+  const raceHighscoreColumn = fileAsText.split("\n").map((row) =>
+    row
+      .split(" ")
+      .filter((f) => !!f)
+      .slice(1)
+  );
+
+  const time = raceHighscoreColumn[0].join("");
+  const distance = raceHighscoreColumn[1].join("");
+
+  return {
+    time: Number(time),
+    distance: Number(distance),
+  };
+};
 
 const getRecordSettingButtonPressTime = (raceRecord: raceHighscore) => {
   const { distance: prevRecord, time: raceTime } = raceRecord;
@@ -72,30 +64,32 @@ const multiplyListTogether = (list: number[]) => {
 
   return sum;
 };
-
-test("AoC 2023 day 6 answer", async () => {
-  const data = await getInputList(inputFile);
-  console.log(data);
-
-  const waysToBeatHighscore: number[] = [];
-
-  data?.forEach((race) => {
-    waysToBeatHighscore.push(getRecordSettingButtonPressTime(race));
-  });
-
-  console.log(waysToBeatHighscore);
-
-  console.log("answer = ", multiplyListTogether(waysToBeatHighscore));
+test("Aoc 2023 day 6 Part 2 answer", async () => {
+  const data = await getPart2InputList(inputFile);
+  const waysToBeatHighscore = getRecordSettingButtonPressTime(data);
+  console.log("Part 2 answer=", waysToBeatHighscore);
 });
 
-test("AoC 2023 day 6 example", async () => {
-  const data = await getInputList(exampleFile);
+// test("Aoc 2023 day 6 Part 2 example", async () => {
+//   const data = await getPart2InputList(exampleFile);
+//   const waysToBeatHighscore = getRecordSettingButtonPressTime(data);
+//   expect(waysToBeatHighscore).toBe(71503);
+// });
 
-  const waysToBeatHighscore: number[] = [];
+// test("AoC 2023 day 6 Part 1 answer", async () => {
+//   const data = await getInputList(inputFile);
+//   const waysToBeatHighscore: number[] = [];
+//   data?.forEach((race) => {
+//     waysToBeatHighscore.push(getRecordSettingButtonPressTime(race));
+//   });
+//   console.log("part 1 answer = ", multiplyListTogether(waysToBeatHighscore));
+// });
 
-  data?.forEach((race) => {
-    waysToBeatHighscore.push(getRecordSettingButtonPressTime(race));
-  });
-
-  expect(multiplyListTogether(waysToBeatHighscore)).toBe(288);
-});
+// test("AoC 2023 day 6 example", async () => {
+//   const data = await getInputList(exampleFile);
+//   const waysToBeatHighscore: number[] = [];
+//   data?.forEach((race) => {
+//     waysToBeatHighscore.push(getRecordSettingButtonPressTime(race));
+//   });
+//   expect(multiplyListTogether(waysToBeatHighscore)).toBe(288);
+// });
